ba818ac59df6be5fb9866feebd4da6a9343f7243
Carguy18 <aaronkobelsky@gmail.com>
2017-03-07 21:38:05 -0700

    Tests are now parameterized

--- src/org/jfree/data/test/range/intersects/ElseBranchCoverageTest.java
+++ src/org/jfree/data/test/range/intersects/ElseBranchCoverageTest.java
@@ -2,20 +2,47 @@ package org.jfree.data.test.range.intersects;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.jfree.data.Range;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith (Parameterized.class)
 public class ElseBranchCoverageTest {
 
-private Range r;
+	private Range r;
+	double lower;
+	double upper;
+	boolean expected;
 	
 	/**
 	 *	Creates an Range from -5.0 to 5.0.
 	 */
-	
 	@Before
 	public void setup() { r = new Range(-5.0, 5.0); }
+	
+	public ElseBranchCoverageTest(double lower, double upper, boolean expected){
+		this.lower = lower;
+		this.upper = upper;
+		this.expected = expected;
+	}
+	
+	@Parameters
+	public static Collection<Object []> data()
+	{
+		return Arrays.asList(new Object[][]
+		{
+			{0, 4, true},
+			{7, 5, false},
+			{7, 10, false},
+			{10, 9, false}
+		});
+	}
 
 	/**
 	 * Test Plan Coverage: intersect test which covers the "else" branch
@@ -27,7 +54,7 @@ private Range r;
 	 */
 	@Test
 	public void intersects_ElseBranch_Test() {
-		assertFalse("Testing \"else\" branch of intersects. Expect false.", r.intersects(10, 15) );
+		assertTrue("Testing \"else\" branch of intersects. Expect " + expected, r.intersects(lower, upper) == expected );
 	}
 
 }
--- src/org/jfree/data/test/range/intersects/IfBranchCoverageTest.java
+++ src/org/jfree/data/test/range/intersects/IfBranchCoverageTest.java
@@ -2,21 +2,46 @@ package org.jfree.data.test.range.intersects;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.jfree.data.Range;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith (Parameterized.class)
 public class IfBranchCoverageTest {
 	
 	private Range r;
+	double lower;
+	double upper;
+	boolean expected;
 	
 	/**
 	 *	Creates an Range from -5.0 to 5.0.
 	 */
-	
 	@Before
 	public void setup() { r = new Range(-5.0, 5.0); }
+	
+	public IfBranchCoverageTest(double lower, double upper, boolean expected){
+		this.lower = lower;
+		this.upper = upper;
+		this.expected = expected;
+	}
 
+	@Parameters
+	public static Collection<Object []> data()
+	{
+		return Arrays.asList(new Object[][]
+		{
+			{-6, 4, true},
+			{-6, -5.5, false}
+		});
+	}
+	
 	/**
 	 * Test Plan Coverage: intersect test which covers the "if" branch
 	 * Test Strategy Coverage: Given range has lower<=this.lower
@@ -27,7 +52,7 @@ public class IfBranchCoverageTest {
 	 */
 	@Test
 	public void intersects_IfBranch_Test() {
-		assertTrue("Testing \"if\" branch of intersects. Expect true.", r.intersects(-6.0, 4) );
+		assertTrue("Testing \"if\" branch of intersects. Expect true.", r.intersects(lower, upper)==expected );
 	}
 
 }
